29d8fc87eba3a4be87be75a9ecd5ba0c585593ab
Paul <118978940+plcr9@users.noreply.github.com>
2023-07-24 17:44:07 +0100

    Update test_error.py

--- test_error.py
+++ test_error.py
@@ -1,1 +1,13 @@
+destinations = {
+  "BUD": 'Budapest',
+  "CMN": 'Casablanca',
+  "IST": 'Istanbul'
+}
+print('Welcome to Small World Airlines!')
+print('What is the airport code of your travel destination?')
+destination = "HND"
 
+assert destination in destinations, "Sorry, small world currently does not fly to this destination!"
+
+city_name = destinations[destination]
+print('Great! Retrieving information for your flight to ...' + city_name)
